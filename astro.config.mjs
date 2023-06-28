aeb609ffab4501a75cda61843035b706ad2d5e44
Daniele Irsuti <irsooti+code@gmail.com>
2023-06-28 15:51:48 +0200

    feat: add "numbers"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), image()]
 });
