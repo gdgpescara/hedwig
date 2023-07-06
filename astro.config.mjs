edfe54ad2c23f4fadcc474a26e605019226feb3a
Daniele Irsuti <irsooti+code@gmail.com>
2023-07-06 19:05:38 +0200

    feat: style fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-
-import image from '@astrojs/image';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gdgpescara.github.io', // TODO: remove after setting the real deploy
-  base: '/hedwig', // TODO: remove after setting the real deploy
-  integrations: [react(), tailwind(), image()],
+  // site: 'https://gdgpescara.github.io', // TODO: remove after setting the real deploy
+  // base: '/hedwig', // TODO: remove after setting the real deploy
+  integrations: [
+    react(),
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,32 +1,32 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin')
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    theme: {
-        extend: {
-            fontFamily: {
-                'scriptina': ['Scriptina'],
-                'harrypotter': ['Harrypotter'],
-            }
-        },
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        scriptina: ["Scriptina"],
+        harrypotter: ["Harrypotter"],
+      },
     },
-    plugins: [
-        require("daisyui"),
-        plugin(function ({addBase}) {
-            addBase({
-                '@font-face': {
-                    fontFamily: 'Scriptina',
-                    fontWheight: '300',
-                    src: 'url(fonts/scriptina.ttf)'
-                }
-            })
-            addBase({
-                '@font-face': {
-                    fontFamily: 'Harrypotter',
-                    src: 'url(fonts/harryp.ttf)'
-                }
-            })
-        }),
-    ],
+  },
+  plugins: [
+    require("daisyui"),
+    plugin(function ({ addBase }) {
+      addBase({
+        "@font-face": {
+          fontFamily: "Scriptina",
+          fontWheight: "300",
+          src: "url(fonts/scriptina.ttf)",
+        },
+      });
+      addBase({
+        "@font-face": {
+          fontFamily: "Harrypotter",
+          src: "url(fonts/harryp.ttf)",
+        },
+      });
+    }),
+  ],
 };
