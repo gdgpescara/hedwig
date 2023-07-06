f42a604af8834ba69533318ddb21b7831fcd87a1
Camillo Bucciarelli <af35609@enel.com>
2023-07-06 17:42:48 +0200

    feat: hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,20 +2,31 @@
 const plugin = require('tailwindcss/plugin')
 
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require("daisyui"),
-    plugin(function ({ addBase }) {
-      addBase({
-        '@font-face': {
-            fontFamily: 'Scriptina',
-            fontWheight: '300',
-            src: 'url(fonts/scriptina.ttf)'
-        }
-      })
-    }),
-  ],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                'scriptina': ['Scriptina'],
+                'harrypotter': ['Harrypotter'],
+            }
+        },
+    },
+    plugins: [
+        require("daisyui"),
+        plugin(function ({addBase}) {
+            addBase({
+                '@font-face': {
+                    fontFamily: 'Scriptina',
+                    fontWheight: '300',
+                    src: 'url(fonts/scriptina.ttf)'
+                }
+            })
+            addBase({
+                '@font-face': {
+                    fontFamily: 'Harrypotter',
+                    src: 'url(fonts/harryp.ttf)'
+                }
+            })
+        }),
+    ],
 };
