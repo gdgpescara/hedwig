7e358c5fe06bb91030514f5fd395c46c0710bc38
Daniele Irsuti <irsooti+code@gmail.com>
2023-09-06 19:16:18 +0200

    feat: add i18n, ssg by default, ssr on demand

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+  defaultLocale: "en",
+  locales: ["en", "it"],
+  defaultNamespace: "common",
+  namespaces: ["common"],
+  // i18nextServer: {
+  //   debug: true,
+  // }
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import nodejs from "@astrojs/node";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gdgpescara.github.io", // TODO: remove after setting the real deploy
   base: '/hedwig', // TODO: remove after setting the real deploy
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(),astroI18next()],
+  output: "hybrid",
+  adapter: nodejs({
+    mode: "middleware",
+  }),
 });
