ae8e93b38ea46fd47cecea075c0d0c6465ed9836
Daniele Irsuti <irsooti+code@gmail.com>
2023-09-10 01:32:29 +0200

    feat: translates contents

--- src/data/nav-data.js
+++
@@ -1,9 +0,0 @@
-const navData = [
-    { name: "Sign up", path: "#signUp" },
-    { name: "Numbers", path: "#numbers" },
-    { name: "Location", path: "#location" },
-    { name: "Sponsors", path: "#sponsors" },
-    { name: "Contacts", path: "#contacts" }
-];
-
-export default navData;
