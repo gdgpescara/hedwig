a1a76612aa8a71abf1c895e1d57436bd7e50799e
Giorgio Campea <campeagiorgio@gmail.com>
2023-10-11 17:54:49 +0200

    first commit

---
+++ src/data/agenda.js
@@ -0,0 +1,57 @@
+const agenda = [
+    {
+        time: '08:30',
+        name: 'Registration',
+        length: '1 hour'
+    },
+    {
+        time: '09:30',
+        name: 'Breakfast',
+        length: '15 mins'
+    },
+    {
+        time: '09:45',
+        name: 'Event opening',
+        length: '15 mins'
+    },
+    {
+        time: '09:45',
+        name: 'Coffee break',
+        length: '15 mins'
+    },
+    {
+        time: '10:15',
+        name: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias recusandae soluta iste veritatis consequuntur dolor rerum accusantium non, illum fuga maxime doloremque',
+        length: '45 mins',
+        language: 'Italian',
+        speaker: {
+            name: 'Jane Doe',
+            description: 'description',
+            image: '/url-di-prova',
+        }
+    },
+    {
+        time: '10:15',
+        name: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates recusandae distinctio repellat vel consequatur commodi nostrum sapiente ad, quibusdam facere animi consequuntur maiores odit in iusto voluptatibus blanditiis, exercitationem adipisci!',
+        length: '45 mins',
+        language: 'English',
+        speaker: {
+            name: 'John Doe',
+            description: 'description',
+            image: '/url-di-prova',
+        }
+    },
+    {
+        time: '11:00',
+        name: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates recusandae distinctio repellat vel consequatur commodi nostrum sapiente ad, quibusdam facere animi consequuntur maiores odit in iusto voluptatibus blanditiis, exercitationem adipisci!',
+        length: '45 mins',
+        language: 'English',
+        speaker: {
+            name: 'John Doe',
+            description: 'description',
+            image: '/url-di-prova',
+        }
+    },
+];
+
+export default agenda;
---
+++ src/data/nav-data.js
@@ -0,0 +1,8 @@
+import { t } from "i18next";
+
+const navData = [
+    { name: t("sign-up"), path: "" },
+    { name: t("agenda"), path: "/agenda" }
+  ];
+
+export default navData;
