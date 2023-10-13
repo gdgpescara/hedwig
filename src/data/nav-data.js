c5b34724d8a27c502b5a11973333fbb55fa1d4b7
Giorgio Campea <campeagiorgio@gmail.com>
2023-10-13 18:21:39 +0200

    created single speaker and speakers page

--- src/data/nav-data.js
+++ src/data/nav-data.js
@@ -3,7 +3,7 @@ import { t } from "i18next";
 const navData = [
   { name: t("sign-up"), path: "/" },
   { name: t("agenda"), path: "/agenda" },
-  { name: t("speakers"), path: "/speakers" }
+  { name: t("speakers"), path: "/speaker" }
 ];
 
 export default navData;
