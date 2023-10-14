0b0e098685c128bad9fa1dfd3a80e909fde911ac
Giorgio Campea <campeagiorgio@gmail.com>
2023-10-14 12:48:49 +0200

    speaker page first commit

--- src/data/speakers-data.ts
+++ src/data/speakers-data.ts
@@ -2,8 +2,17 @@ export const speakersData = {
   "f-sciuti": {
     name: "Francesco Sciuti",
     imageUrl:
-      "https://pbs.twimg.com/profile_images/1410277875389127680/6X1X1J9U_400x400.jpg",
-    description: "Francesco is a software engineer at Pika.",
+      "https://media.licdn.com/dms/image/C5603AQEdipsEgu67Aw/profile-displayphoto-shrink_800_800/0/1576755639170?e=2147483647&v=beta&t=_grhWifPhppnY7AutryxrOgLUMm4eIAfa_sPxni_OaU",
+    work: "Devmy",
+    jobTitle: "CEO",
+    description: "CEO @Devmy, DevRel Expert @Codemotion, Google Developer Expert (GDE), Microsoft MVP, Google Certified Developer, LEGO Serious Play Facilitator, Projects Manager, Software Engineer, Speaker/Evangelist/Trainer, AWS UG Lead.",
+    social: {
+      instagram: "",
+      facebook: "",
+      linkedin: "https://it.linkedin.com/in/francescosciuti",
+      twitter: "",
+      sito: "https://www.francescosciuti.it/"
+    }
   },
   "f-biondi": {
     name: "Fabio Biondi",
