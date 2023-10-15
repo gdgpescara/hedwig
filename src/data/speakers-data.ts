a8db8f42592fa4a761a6571365132ec377b9c6e7
Giorgio Campea <campeagiorgio@gmail.com>
2023-10-15 18:37:17 +0200

    fix: speaker

--- src/data/speakers-data.ts
+++ src/data/speakers-data.ts
@@ -2,24 +2,41 @@ export const speakersData = {
   "f-sciuti": {
     name: "Francesco Sciuti",
     imageUrl:
-      "https://media.licdn.com/dms/image/C5603AQEdipsEgu67Aw/profile-displayphoto-shrink_800_800/0/1576755639170?e=2147483647&v=beta&t=_grhWifPhppnY7AutryxrOgLUMm4eIAfa_sPxni_OaU",
+      "https://sessionize.com/image/7fb1-400o400o2-42-456a-4934-a1ac-414d4ba2990b.eb67300d-90b9-411f-ae4d-e3cfc44f9075.png",
     company: "Devmy",
-    jobTitle: "CEO",
-    description: "CEO @Devmy, DevRel Expert @Codemotion, Google Developer Expert (GDE), Microsoft MVP, Google Certified Developer, LEGO Serious Play Facilitator, Projects Manager, Software Engineer, Speaker/Evangelist/Trainer, AWS UG Lead.",
-    bio: "",
+    jobTitle: "CEO - Google Developer Expert, Microsoft MVP, Google Certified Developer & AWS UG Lead",
+    description: "CEO @Devmy - Google Developer Expert, Microsoft MVP, Google Certified Developer & AWS UG Lead",
+    bio: `Francesco (Google Developer Expert, Microsoft MVP, Google Certified Developer & AWS UG Lead), is a programmer with several years of experience in enterprise programming , high skilled in web development (frontend and backend) and in team project management.
+    I have a keen interest in new technologies and in IT development and I'm always upgrading and evaluating my knowledge and skills to stay at the cutting-edge of web development.
+    My outstanding verbal and written communication skills let me involved in lots of meetings, workshop and courses both in Sicily and in Italy as a speaker and tech event organizer.
+    Focused on continual professional and technological improvement: books, courses, articles, forums and regular attender of tech events where I can meet new and interesting people with which exchange as much as possible ideas and technical knowledge.
+    I want to put myself in challenges both in the personal and professional side and this is way I'm involved ,as an active contributor, in many Italian communities (Angular ,React, Javascript ,GDG, PUG, etc…) and I contribute as a counselor to CoderDojo Foundation (Computational youth learning).
+    I've worked for a long time as a employee and then as a freelancer, but nowadays I has my own company: Acadevmy - Software Factory, founded with my friends Andrea and Salvo.`,
     social: {
       instagram: "",
       facebook: "",
-      linkedin: "https://it.linkedin.com/in/francescosciuti",
-      twitter: "",
-      sito: "https://www.francescosciuti.it/"
+      linkedin: "https://www.linkedin.com/in/francescosciuti/",
+      twitter: "https://twitter.com/FrancescoSciuti",
+      blog: "https://devmy.it/blog"
     }
   },
   "f-biondi": {
     name: "Fabio Biondi",
     imageUrl:
-      "https://pbs.twimg.com/profile_images/1410277875389127680/6X1X1J9U_400x400.jpg",
-    description: "Fabio is a software engineer at Pika.",
+      "https://sessionize.com/image/5dbc-400o400o2-64-8de2-4e9c-9766-14a527f8820c.b46b3700-6eeb-4a6f-857c-7ee0c024ee00.png",
+    description: "Microsoft MVP | Google Expert in Angular",
+    company: "https://www.fabiobiondi.dev/",
+    jobTitle: "Microsoft MVP | Google Expert in Angular",
+    bio: `Fabio Biondi, Microsoft MVP e Google Developer Expert, è uno sviluppatore freelance con più di 15 anni di esperienza nella creazione di applicazioni web enterprise.
+    Speaker in numerosi eventi front-end, ha inoltre fondato diverse community italiane che si occupano di Javascript, Angular e React (~30000 membri).
+    Attualmente si occupa principalmente di formazione e consulenza su JS, TS, Angular, React, Redux, NGRX, Next e RxJS.`,
+    social: {
+      instagram: "https://www.instagram.com/biondifabio/",
+      facebook: "",
+      linkedin: "https://www.linkedin.com/in/fabiobiondi/",
+      twitter: "https://twitter.com/biondifabio",
+      blog: "https://www.francescosciuti.it/"
+    }
   },
   "g-palama": {
     name: "Gregorio Palamà",
@@ -31,7 +48,7 @@ export const speakersData = {
     name: "Giovanna Reggina Galleno Malaga",
     imageUrl:
       "https://sessionize.com/image/c226-400o400o2-9Qrv5Z9ePxyKxEQKsQVK48.JPG",
-    description: "",
+    description: "Technical Project Manager & Soft Skills Specialist",
     company: "",
     jobTitle: "Technical Project Manager & Soft Skills Specialist",
     bio: `Giovanna is a Technical Project Manager in BIP xTech. She worked in the European Google Developer Relations team as a Senior Program Manager where she fell in love with the tech world.
@@ -50,8 +67,8 @@ export const speakersData = {
     name: "Daniele Favaro",
     imageUrl:
       "https://sessionize.com/image/fdad-400o400o2-BcdvQRoQMg4YWv2RQ6QXiC.jpg",
-    description: "",
-    company: "EasyPark Group",
+    description: "Software Engineer @EasyPark Group",
+    company: "EasyPark",
     jobTitle: "Software Engineer",
     bio: `Based in Stockholm, Sweden, Daniele is working on the Android stack core solutions that millions of drivers use.
       He has been working with Android since Gingerbread and Eclipse IDE were a thing. 
@@ -68,13 +85,13 @@ export const speakersData = {
     name: "Marco Ippolito",
     imageUrl:
       "https://sessionize.com/image/a7c7-400o400o2-74uC92g6sNWezMZsFAMnj5.jpg",
-    description: "",
+    description: "Developer Experience Engineer @Nearform",
     company: "Nearform",
     jobTitle: "Developer Experience Engineer",
     bio: `With years of experience in designing, developing and testing high scalability and distributed cloud applications, I'm committed to contributing to open source projects and helping the Node.js community grow. Currently serving as a maintainer of Node.js, Fastify and Mercurius and a member of the security work group.`,
     social: {
       instagram: "https://www.instagram.com/ultralightbeamr/",
-      facebook: "https://twitter.com/satanacchio",
+      blog: "https://twitter.com/satanacchio",
       linkedin: "https://www.linkedin.com/in/marcoippolito2021/",
       twitter: "https://twitter.com/satanacchio",
     }
@@ -83,7 +100,7 @@ export const speakersData = {
     name: "Fabio Carusi",
     imageUrl:
       "https://sessionize.com/image/6957-400o400o2-nqkvrZXmCdzrwFXfuyn1dv.jpg",
-    description: "Aspirante Nerd, Data Engineer per lavoro, sviluppatore per passione, pigro per natura.",
+    description: "Data Engineer @Capgemini",
     company: "Capgemini",
     jobTitle: "Data Engineer",
     bio: `Aspirante Nerd, Data Engineer per lavoro, sviluppatore per passione, pigro per natura.`,
@@ -92,6 +109,63 @@ export const speakersData = {
       facebook: "",
       linkedin: "https://www.linkedin.com/in/fabio-carusi-34812325/",
       twitter: "",
+    }
+  },
+  "s-erradi": {
+    name: "Soumaya Erradi",
+    imageUrl:
+      "https://sessionize.com/image/4136-400o400o2-9xJYxgvnNyvDJpcpmnJ49R.jpg",
+    description: "Lead Software Developer",
+    company: "",
+    jobTitle: "Lead Software Developer",
+    bio: `I'm Soumaya Erradi and I'm a software developer and an IT teacher. 
+      I'm high skilled in web development (frontend and backend), specialized in developing enterprise applications using Angular.
+      Currently, I'm working as frontend lead developer in a team focused on blockchain app development. 
+      I'm also holding coding courses and meetups to deepen technical and business issues related to Blockchain and Smart Contracts.
+      As a speaker at conferences I bring advanced Angular topics, tips for the integration of smart contracts and best solutions for decentralized applications and web3.`,
+    social: {
+      instagram: "https://www.instagram.com/sumyerradi/",
+      facebook: "",
+      linkedin: "https://www.linkedin.com/in/soumaya-erradi",
+      twitter: "https://twitter.com/sumy92",
+      blog: "https://www.soumayaerradi.it/"
+    }
+  },
+  "j-campbell": {
+    name: "Jai Campbell",
+    imageUrl:
+      "https://sessionize.com/image/da4b-400o400o2-7HuW8Sj4fThNMdDBpWjXv6.jpg",
+    description: "SRE | Wipro Digital",
+    company: "Wipro",
+    jobTitle: "SRE",
+    bio: `I'm Soumaya Erradi and I'm a software developer and an IT teacher. 
+      I'm high skilled in web development (frontend and backend), specialized in developing enterprise applications using Angular.
+      Currently, I'm working as frontend lead developer in a team focused on blockchain app development. 
+      I'm also holding coding courses and meetups to deepen technical and business issues related to Blockchain and Smart Contracts.
+      As a speaker at conferences I bring advanced Angular topics, tips for the integration of smart contracts and best solutions for decentralized applications and web3.`,
+    social: {
+      instagram: "https://www.instagram.com/sumyerradi/",
+      facebook: "",
+      linkedin: "https://www.linkedin.com/in/soumaya-erradi",
+      twitter: "https://twitter.com/sumy92",
+      blog: "https://www.soumayaerradi.it/"
+    }
+  },
+  "m-bertaina": {
+    name: "Michela Bertaina",
+    imageUrl:
+      "https://sessionize.com/image/ef34-400o400o2-9tzuSxBbFx6LZVWkzFi1Pk.png",
+    description: "Community Manager EMEA @ Codemotion| TEDx Cuneo & Italian Community Manager Summit Organizer | WTM Ambassador | GDG Bari Lead | Tourism Councilor @ Cissone",
+    company: "Codemotion",
+    jobTitle: "Community Manager EMEA @ Codemotion| TEDx Cuneo & Italian Community Manager Summit Organizer | WTM Ambassador | GDG Bari Lead | Tourism Councilor @ Cissone",
+    bio: `Community lover and Latin American dancer. In 2017 she began her journey within the tech communities. Currently lead in the GDG Bari, WTM Ambassador, organizer in ICM Summit, lead of the partnership team of TEDxCuneo, tourism councilor of Cissone and in the core team of DInA Tech Hub. Diversity, accessibility, sharing and collaborating are the values ​​on which her projects are based, which she carries out with enthusiasm and great energy.
+      Here are more info about me: https://bit.ly/pantarei-mb-en`,
+    social: {
+      instagram: "https://instagram.com/pantarei_mb",
+      facebook: "",
+      linkedin: "https://www.linkedin.com/in/michela-bertaina",
+      twitter: "https://twitter.com/bertainamichela",
+      blog: "https://sites.google.com/view/pantarei-mb-en/blog"
     }
   },
 };
@@ -112,6 +186,6 @@ export interface Speaker {
     facebook: string;
     linkedin: string;
     twitter: string;
-    sito: string;
+    blog: string;
   }
 }
