78fa10673bb57e54c3ba82af56992c8ebb4b4282
Daniele Irsuti <irsooti+code@gmail.com>
2023-10-15 17:02:12 +0200

    feat: add translations

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -3,7 +3,7 @@ export default {
   defaultLocale: "en",
   locales: ["en", "it"],
   defaultNamespace: "common",
-  namespaces: ["common", "landing"],
+  namespaces: ["common", "landing", "agenda"],
   // i18nextServer: {
   //   debug: true,
   // }
