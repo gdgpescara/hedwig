c4e5ec8abcdeb71935dec60b73f6c4f836f6210a
Daniele Irsuti <irsooti+code@gmail.com>
2024-01-31 17:40:54 +0100

    Remove failing test and update test description

--- src/shared/Dummy/dummy.test.ts
+++ src/shared/Dummy/dummy.test.ts
@@ -3,7 +3,3 @@ import { expect, it } from "vitest";
 it("should pass", () => {
   expect(true).toBe(true);
 });
-
-it("should fail", () => {
-  expect(true).toBe(false);
-});
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,5 @@
 import { test, expect } from "@playwright/test";
 
-test("has heading in the document", async ({ page }) => {
-  await page.goto("/");
-
-  const title = page.getByRole("heading");
-
-  await expect(title).toHaveText("Hedwxig");
-});
-
 test("contains hedwig in the document", async ({ page }) => {
   await page.goto("/");
 
