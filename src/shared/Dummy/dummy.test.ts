a7e9420a4e4379d2d0d77a043ba33fdf51f9b15e
Daniele Irsuti <irsooti+code@gmail.com>
2024-01-31 11:50:18 +0100

    Add vitest.yml and vitest-github-actions-reporter

---
+++ src/shared/Dummy/dummy.test.ts
@@ -0,0 +1,9 @@
+import { expect, it } from "vitest";
+
+it("should pass", () => {
+  expect(true).toBe(true);
+});
+
+it("should fail", () => {
+  expect(true).toBe(false);
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,12 @@
 /// <reference types="vitest" />
 import { getViteConfig } from "astro/config";
+import GithubActionsReporter from "vitest-github-actions-reporter";
 
 export default getViteConfig({
-  test: {},
+  test: {
+    dir: "./src",
+    reporters: process.env.GITHUB_ACTIONS
+      ? new GithubActionsReporter()
+      : "default",
+  },
 });
