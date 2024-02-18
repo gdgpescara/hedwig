73542fa8ec6c124396107a318065eb98ea8345b2
Gregorio Palamà <gregoriopalama@gmail.com>
2024-02-18 02:27:32 +0100

    INFRA07 - Create a service account and configurations for Firebase for the demo application

---
+++ src/firebase/server.ts
@@ -0,0 +1,8 @@
+import type { ServiceAccount } from "firebase-admin";
+import { initializeApp, cert } from "firebase-admin/app";
+
+const serviceAccount = JSON.parse(import.meta.env.FIREBASE_SERVICE_ACCOUNT || {});
+
+export const app = initializeApp({
+  credential: cert(serviceAccount as ServiceAccount),
+});
