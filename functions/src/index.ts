042d323192c38bb30702c81d1332608713d06668
Gregorio Palamà <gregoriopalama@gmail.com>
2024-03-07 22:55:51 +0100

    fix for ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,3 @@
-import * as glob from "glob";
-import camelCase from "camelcase";
+import * as http_api from './api/http/api';
 
-const functionFiles = glob.sync('./api/**/*.f.js', { cwd: __dirname, ignore: './node_modules/**'});
-
-for (let i = 0; i < functionFiles.length; i++) {
-  const functionFile = functionFiles[i];
-
-  const functionName = camelCase(
-    functionFile.split('.').slice(0, -2).join().split('/').join('-')
-  ); 
-
-  if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === functionName) {
-    const module = require(functionName);
-    exports[functionName] = module.default || module;
-  }
-}
+export { http_api };
