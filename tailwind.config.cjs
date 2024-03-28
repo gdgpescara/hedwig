1215a2b527d699392a072919cbbfb9a7ff67b1b0
Gregorio Palamà <gregoriopalama@gmail.com>
2024-03-28 14:05:28 +0100

    INFRA12 - Define a budget for Accessibility score in Lighthouse check (#147)
    * INFRA12 - Define a budget for Accessibility score in Lighthouse check
    
    * Updated the theme to add contrast
    
    * lighthouse-ci 11

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["dark"],
+    themes: [
+      {
+        hedwig: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          "primary": "#000000",
+          "secondary": "#FFF",
+          "neutral": "#FFF",
+          "base-content": "#FFF"
+        },
+      },
+    ],
   },
 };
