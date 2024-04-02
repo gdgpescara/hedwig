12a726a8a3cc721c3494f4e33bcf5e917cfaf612
Gregorio Palamà <gregoriopalama@gmail.com>
2024-04-03 01:20:29 +0200

    default firebase config

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,7 +1,7 @@
 import type { ServiceAccount } from "firebase-admin";
 import { initializeApp, cert } from "firebase-admin/app";
 
-const serviceAccount = JSON.parse(import.meta.env.PUBLIC_FIREBASE_SERVICE_ACCOUNT || {});
+const serviceAccount = JSON.parse(import.meta.env.PUBLIC_FIREBASE_SERVICE_ACCOUNT || "{}");
 
 export const app = initializeApp({
   credential: cert(serviceAccount as ServiceAccount),
