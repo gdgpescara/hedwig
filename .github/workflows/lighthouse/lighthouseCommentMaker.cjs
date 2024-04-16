fe06a6000963f81e18ce9e0ba1aa066bd9f4f778
Gregorio Palamà <gregoriopalama@gmail.com>
2024-04-16 14:38:29 +0200

    INFRA10 - Define a budget for SEO score in Lighthouse check (#158)
    * INFRA10 - Define a budget for SEO score in Lighthouse check
    
    * Restored accessibility
    
    * test lighthouse summary
    
    * testing seo
    
    * different lighthouse settings
    
    * all runs are in the report
    
    * testing assertions
    
    * passing assertionResults
    
    * printing output
    
    * changed config
    
    * no budget
    
    * cwv in assertions
    
    * lowering seo score for testing purposes
    
    * testing lhci outputs
    
    * correct value for LCP
    
    * include passed assertions in report
    
    * json report
    
    * lighthouse comment is calculated over all the runs
    
    * seo requires minscore 1
    
    * includePassedAssertions to true
    
    * test includePassedAssertions
    
    * logging assertionResults
    
    * SEO back to 1
    
    * viewport and description

--- .github/workflows/lighthouse/lighthouseCommentMaker.cjs
+++ .github/workflows/lighthouse/lighthouseCommentMaker.cjs
@@ -19,9 +19,21 @@
  */
 
 /**
+ * @typedef {Object} AssertionResult
+ * @prop {string} name
+ * @prop {boolean} passed
+ * @prop {number} expected
+ * @prop {number} actual
+ * @prop {number[]} values
+ * @prop {string} auditProperty
+ * @prop {string} operator
+ */
+
+/**
  * @typedef {Object} LighthouseOutputs
  * @prop {Record<string, string>} links
  * @prop {Manifest[]} manifest
+ * @prop {AssertionResult[]} assertionResults
  */
 
 const formatScore = (/** @type { number } */ score) => Math.round(score * 100);
@@ -37,22 +49,39 @@ const scoreRow = (
  * @param {LighthouseOutputs} lighthouseOutputs
  */
 function makeComment(lighthouseOutputs) {
-  const { summary } = lighthouseOutputs.manifest[0];
+  const manifest = lighthouseOutputs.manifest;
   const [[testedUrl, reportUrl]] = Object.entries(lighthouseOutputs.links);
 
-  const comment = `## ⚡️🏠 Lighthouse report
+  const runs = manifest.length;
+  let metrics = {
+    "performance" : new Array(),
+    "accessibility" : new Array(),
+    "bestpractices" : new Array(),
+    "seo" : new Array()
+  };
+
+  manifest.map(m => m.summary)
+    .forEach(summary => {
+        metrics["performance"].push(summary.performance);
+        metrics["accessibility"].push(summary.accessibility);
+        metrics["bestpractices"].push(summary["best-practices"]);
+        metrics["seo"].push(summary.seo);
+    });
+
+  let comment = `## ⚡️🏠 Lighthouse report
 
 We ran Lighthouse against the changes and produced this [report](${reportUrl}). Here's the summary:
 
 | Category | Score |
 | -------- | ----- |
-${scoreRow('Performance', summary.performance)}
-${scoreRow('Accessibility', summary.accessibility)}
-${scoreRow('Best practices', summary['best-practices'])}
-${scoreRow('SEO', summary.seo)}
-${scoreRow('PWA', summary.pwa)}
+${scoreRow('Performance', metrics.performance.reduce((acc, p) => acc + p, 0) / metrics.performance.length)}
+${scoreRow('Accessibility', Math.max(...metrics.accessibility))}
+${scoreRow('Best practices', Math.max(...metrics.bestpractices))}
+${scoreRow('SEO', Math.max(...metrics.seo))}
 
 *Lighthouse ran against [${testedUrl}](${testedUrl})*
+
+
 `;
 
   return comment;
