1025458821147c78b3f9975d2ede4f006619d00a
Gregorio Palamà <gregoriopalama@gmail.com>
2024-04-17 10:15:49 +0200

    test with auth in client

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,5 +1,8 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = JSON.parse(import.meta.env.PUBLIC_FIREBASE_CONFIG || "{}");
 
 export const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
