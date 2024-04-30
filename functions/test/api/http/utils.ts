638abfb7161c22c631dafa0874c3420c262a9d13
Gregorio Palamà <gregoriopalama@gmail.com>
2024-04-30 23:44:26 +0200

    Using lookpack IP instead of DNS

--- functions/test/api/http/utils.ts
+++ functions/test/api/http/utils.ts
@@ -1,6 +1,6 @@
 import { functionsRegion, projectId } from "../../../src/config";
 
-export const httpApiBaseUrl = `http://localhost:5001/${projectId}/${functionsRegion}/http_api`;
+export const httpApiBaseUrl = `http://127.0.0.1:5001/${projectId}/${functionsRegion}/http_api`;
 
 export const login = async (
   email: string,
@@ -10,7 +10,7 @@ export const login = async (
   idToken: string;
 }> => {
   const response = await fetch(
-    "http://localhost:9099/identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=12345",
+    "http://127.0.0.1:9099/identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=12345",
     {
       method: "POST",
       headers: {
